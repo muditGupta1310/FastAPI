68e40537fd7025df7a85abaf0104a0c63b7a6c85
muditGupta1310 <techskill2007@gmail.com>
2025-07-17 02:08:42 +0530

    Hello World + about API

--- hello_world_api.py
+++ hello_world_api.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello world!"}
+
+
+@app.get('/about')
+def about():
+    return {'message':'FastAPI uses a ASGI (Asynchronous Server Gateway Interface) server to handle requests. This allows for asynchronous programming, which can improve performance for I/O-bound operations.'}
