65fa63ee7afbc73e95d0261ab8e8d67707eb89f9
muditGupta1310 <techskill2007@gmail.com>
2025-07-18 01:59:19 +0530

    added Path param in code2.py file

--- code2.py
+++ code2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -23,3 +23,10 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get('/view/{patient_id}')
+def view_patient(patient_id: str = Path(...,description="ID of the patient in DB", example="P001")):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {"error": "Patient not found"}, 404
