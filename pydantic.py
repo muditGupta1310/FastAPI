ec3b23f1291f0ac45332b49d5549ca8c483c461a
muditGupta1310 <techskill2007@gmail.com>
2025-07-22 00:17:58 +0530

    pydantic file created

---
+++ pydantic.py
@@ -0,0 +1,4 @@
+# Why Pydantic is used?
+# Pydantic is used in FastAPI to define data models and validate request and response data. It ensures that the data adheres to the specified types and constraints, providing automatic serialization and deserialization of complex data structures. This helps in maintaining data integrity and simplifies the development process by reducing boilerplate code for data validation.
+
+
