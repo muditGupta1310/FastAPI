657460f04fa9b34827b6f35d9679b29ecaa6030c
muditGupta1310 <techskill2007@gmail.com>
2026-01-05 23:21:44 +0530

    validations and error handling

--- CRUD OPERATIONS/models_val.py
+++ CRUD OPERATIONS/models_val.py
@@ -6,7 +6,3 @@ class Employee(BaseModel):
     name:str = Field(..., min_length=3, max_length=50, regex=r'^[A-Za-z]+(\.[A-Za-z]+)*$')
     department:str = Field(..., min_length=2, max_length=50, regex=r'^[A-Za-z]+([ .-][A-Za-z]+)*$')
     age: Optional[int] = Field(default=None, ge=18, le=100)
-
-
-
-
