75c34b65b48aa0c947b976965f07e03ca1dcd35c
muditGupta1310 <techskill2007@gmail.com>
2026-01-05 23:46:34 +0530

    handling validation and erros

--- CRUD OPERATIONS/models_val.py
+++ CRUD OPERATIONS/models_val.py
@@ -3,6 +3,6 @@ from typing import Optional
 
 class Employee(BaseModel):
     id:int = Field(..., gt=0)
-    name:str = Field(..., min_length=3, max_length=50, regex=r'^[A-Za-z]+(\.[A-Za-z]+)*$')
-    department:str = Field(..., min_length=2, max_length=50, regex=r'^[A-Za-z]+([ .-][A-Za-z]+)*$')
+    name:str = Field(..., min_length=3, max_length=50)
+    department:str = Field(..., min_length=2, max_length=50)
     age: Optional[int] = Field(default=None, ge=18, le=100)
