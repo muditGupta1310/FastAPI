5dc464127e2ee8428df6d6785876652b6d6b669a
muditGupta1310 <techskill2007@gmail.com>
2026-01-08 23:51:17 +0530

    crud-app

---
+++ crud-app/database.py
@@ -0,0 +1,15 @@
+# Help setup database connection and provide foundational components for ORM
+# Centralizes DB setup, making it reusable across the app for sessions and models
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False}
+)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
---
+++ crud-app/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, CheckConstraint
+from database import Base
+
+class Employee(Base):
+    __tablename__ = 'employees'
+    id = Column(Integer,CheckConstraint('id > 0',name='id_gt_zero'), primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
