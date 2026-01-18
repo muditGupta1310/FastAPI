4eaa17b2599d417e986c57e53b061c429a9f64cd
muditGupta1310 <techskill2007@gmail.com>
2026-01-18 13:12:01 +0530

    ML Serialization

--- Model Serialization/ML Integration/main.py
+++ Model Serialization/ML Integration/main.py
@@ -2,7 +2,8 @@
 
 from fastapi import FastAPI
 from schemas import InputSchema, OutputSchema
-from predict import make_predications
+from predict import make_predications,make_batch_predictions
+from typing import List
 
 
 app = FastAPI()
@@ -15,3 +16,9 @@ def index():
 def predict(user_input : InputSchema):
     prediction = make_predications(user_input.model_dump())
     return OutputSchema(predicted_price=round(prediction,2))
+
+
+@app.post('/batch_prediction', response_model=List[OutputSchema])
+def batch_predict(user_input:List[InputSchema]):
+    predictions = make_batch_predictions([x.model_dump() for x in user_input])
+    return [OutputSchema(predicted_price = round(prediction,2)) for prediction in predictions]
--- Model Serialization/ML Integration/predict.py
+++ Model Serialization/ML Integration/predict.py
@@ -2,6 +2,7 @@
 
 import joblib
 import numpy as np
+from typing import List
 
 model = joblib.load('model.joblib')
 print('Loaded the model')
@@ -16,8 +17,25 @@ def make_predications(data:dict) -> float:
             data['total_bedrooms'],
             data['population'],
             data['households'],
-            data['median_income'],
+            data['median_income']
         ]
     ])
     return model.predict(features)[0]
     
+
+def make_batch_predictions(data:List[dict]) -> np.array:
+    X = np.array([
+        [
+            x['longitude'],
+            x['latitude'],
+            x['housing_median_age'],
+            x['total_rooms'],
+            x['total_bedrooms'],
+            x['population'],
+            x['households'],
+            x['median_income']
+        ]
+        for x in data
+    ])
+    print(X)
+    return model.predict(X)
